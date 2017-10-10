d5134ec3b43d5be5927e0ea7da006e51abc2cc5a
Aleksandr Talalaev <neuotq@gmail.com>
2017-10-10 19:01:36 +0300

    Most + fixes

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -87,6 +87,35 @@ $("document").ready(function() {
     // $("#carousel-testimonials").css("height", 340);
 });
 
+function carouselNormalization() {
+var items = $('#carouselExampleIndicators .carousel-item'), //grab all slides
+    heights = [], //create empty array to store height values
+    tallest; //create variable to make note of the tallest slide
+
+if (items.length) {
+    function normalizeHeights() {
+        items.each(function() { //add heights to array
+            heights.push($(this).height()); 
+        });
+        tallest = Math.max.apply(null, heights); //cache largest value
+        items.each(function() {
+            $(this).css('min-height',tallest + 'px');
+        });
+    };
+    normalizeHeights();
+
+    $(window).on('resize orientationchange', function () {
+        tallest = 0, heights.length = 0; //reset vars
+        items.each(function() {
+            $(this).css('min-height','0'); //reset min-height
+        }); 
+        normalizeHeights(); //run it again 
+    });
+}
+}
+
+
+
 $("document").ready(function() {
 var maxHeight = -1;
 $('#carousel-testimonials .carousel-item').each(function() {
@@ -95,6 +124,8 @@ $('#carousel-testimonials .carousel-item').each(function() {
     }
 });
 $('.cc-testimonialsbox').height(maxHeight);
+
+carouselNormalization();
 
 });
 
